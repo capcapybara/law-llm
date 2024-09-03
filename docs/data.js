da73cb5c694b825406a86e4c0853463f6c8404b5
github-action-benchmark <github@users.noreply.github.com>
2024-09-03 04:59:57 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 69a99e332296dfdf11560fa3f238c5e5a0b08739

---
+++ docs/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1725339597075,
+  "repoUrl": "https://github.com/capcapybara/law-llm",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "meen.sankaew@gmail.com",
+            "name": "msp5382",
+            "username": "msp5382"
+          },
+          "committer": {
+            "email": "meen.sankaew@gmail.com",
+            "name": "msp5382",
+            "username": "msp5382"
+          },
+          "distinct": true,
+          "id": "69a99e332296dfdf11560fa3f238c5e5a0b08739",
+          "message": "fix: ci",
+          "timestamp": "2024-09-03T11:59:12+07:00",
+          "tree_id": "a25ff84c451d9f0ad769e9dc22d3d2acca7cf871",
+          "url": "https://github.com/capcapybara/law-llm/commit/69a99e332296dfdf11560fa3f238c5e5a0b08739"
+        },
+        "date": 1725339596658,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Result Average Similarity",
+            "value": 92.32404666725652,
+            "unit": "Percent"
+          }
+        ]
+      }
+    ]
+  }
+}
